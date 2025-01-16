847f498561ea792c7daf53b31375d545267fb55b
Lucas Rodriguez <lucasg29r95@gmail.com>
2025-01-16 16:31:25 -0300

    Funciones ordenadas

--- app.js
+++ app.js
@@ -2,6 +2,31 @@
 
 let friendsList = [];
 
+function agregarAmigo() {
+    let newFriend = document.getElementById('amigo').value;
+    
+    if(newFriend !== '') {
+        friendsList.push(newFriend);
+    } else {
+        alert('Por favor, inserte un nombre.');
+    }
+
+    document.getElementById('amigo').value = "";
+    actualizarListaAmigos();
+}
+
+function actualizarListaAmigos() {
+    let listaAmigos = document.querySelector('.name-list');
+
+    listaAmigos.innerHTML = "";
+    
+    for(let i = 0; i < friendsList.length; i++) {
+        let item = document.createElement('li');
+        item.textContent = friendsList[i];
+        listaAmigos.appendChild(item);
+    }
+}
+
 function sortearAmigo() {
     let result = document.getElementById('resultado');
 
