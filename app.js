f45ed14c62208f2f93091a5b5a260d13839fb3f8
Lucas Rodriguez <lucasg29r95@gmail.com>
2025-01-16 15:51:35 -0300

    Funcion agregarAmigo añadida

--- app.js
+++ app.js
@@ -1,3 +1,15 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 
 let friendsList = [];
+
+function agregarAmigo() {
+    let newFriend = document.getElementById('amigo').value;
+    
+    if(newFriend !== '') {
+        friendsList.push(newFriend);
+    } else {
+        alert('Por favor, inserte un nombre.');
+    }
+
+    document.getElementById('amigo').value = "";
+}
